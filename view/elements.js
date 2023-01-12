9690c3e900c70d6808d9e95e8b9b3014f6dc6e8c
isaacnwt <isaacnewton02@hotmail.com>
2023-01-12 01:39:33 -0300

    Refatoração das views, modularizando código em constantes

---
+++ view/elements.js
@@ -0,0 +1,7 @@
+export const elements = {
+    input: document.querySelector(".input_adicionar_tarefa"),
+    addButton: document.querySelector(".botao_adicionar_tarefa"),
+    tasksContainer: document.querySelector("#tasks-container")
+}
+
+// constante contendo os seletores utilizados na view e na controller
